b8718ae3eb51cf427513de8a8c0dd8aa1428786e
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-03-01 12:30:35 +0600

    Woring

--- Some Core Animation/View/CoreViewController.swift
+++ Some Core Animation/View/CoreViewController.swift
@@ -8,16 +8,28 @@
 import UIKit
 
 class CoreViewController: UIViewController {
+    
     @IBOutlet weak var redView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
     @IBOutlet weak var menuButton: UIButton!
     @IBOutlet weak var blurImageView: UIImageView!
+    @IBOutlet weak var boderView: UIView!
+    
+    @IBOutlet weak var animatedCountingLabel: UILabel!
+    
+    // MARK: - Properties -
+    
+    var circularProgressBarView: CircularProgressBarView!
+    var circularViewDuration: TimeInterval = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
         menuButton.layer.cornerRadius = 5
-        
+        setUpCircularProgressBarView()
         blurImage()
+        changeColor()
+        borderColor()
+        
         // Do any additional setup after loading the view.
     }
     
@@ -47,5 +59,46 @@ class CoreViewController: UIViewController {
         // 3
         blurImageView.addSubview(blurView)
     }
+    
+    // Change view color
+    func changeColor() {
+        
+        // Define an array of 6 colors
+        let colors: [UIColor] = [.red, .orange, .yellow, .green, .blue, .purple]
+        
+        // Set the initial color of the view
+        boderView.backgroundColor = colors.first
+        
+        // Animate the color change to the rest of the colors in the array
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat, .autoreverse], animations: {
+            for color in colors.dropFirst() {
+                self.boderView.backgroundColor = color
+            }
+        }, completion: nil)
+    }
+    
+    // view border Color
+    
+    func borderColor() {
+        let gradient = CAGradientLayer()
+        gradient.frame =  CGRect(origin: CGPoint.zero, size: self.boderView.frame.size)
+        gradient.colors = [UIColor.blue.cgColor, UIColor.brown.cgColor]
+        
+        let shape = CAShapeLayer()
+        shape.lineWidth = 20
+        shape.path = UIBezierPath(rect: self.boderView.bounds).cgPath
+        shape.strokeColor = UIColor.black.cgColor
+        shape.fillColor = UIColor.clear.cgColor
+        gradient.mask = shape
+        
+        self.boderView.layer.addSublayer(gradient)
+    }
+    
+    func setUpCircularProgressBarView() {
+            circularProgressBarView = CircularProgressBarView(frame: .zero)
+            circularProgressBarView.center = view.center
+            circularProgressBarView.progressAnimation(duration: circularViewDuration)
+            view.addSubview(circularProgressBarView)
+        }
     
 }
